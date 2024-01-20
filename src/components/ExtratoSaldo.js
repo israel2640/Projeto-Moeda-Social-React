489c8251915757756f33e93736bc83a560f4f011
Israel Paz <israelnetmax@gmail.com>
2024-01-20 09:47:30 -0300

    Atualização 1.0.3
    Foi realizado melhorias na página de extrato.

--- src/components/ExtratoSaldo.js
+++ src/components/ExtratoSaldo.js
@@ -1,135 +1,116 @@
-import React, { useState, useEffect, useMemo, useRef } from 'react';
-import Chart from 'chart.js/auto';
+import React, { useState, useMemo } from 'react';
 
 import './ExtratoSaldo.css';
 
 const ExtratoSaldo = () => {
-    const [mesSelecionado, setMesSelecionado] = useState('Janeiro 2024');
-    const canvasRef = useRef(null);
-    const myChartRef = useRef(null);
+  const [mesSelecionado, setMesSelecionado] = useState('Janeiro 2024');
 
-    const extratoData = useMemo(() => {
-        if (mesSelecionado === 'Janeiro 2024') {
-            return [
-                { data: '05-01-2024T10:30:00', descricao: 'Participação em Sala - 10:30 AM', valor: 50.0 },
-                { data: '10-01-2024T15:45:00', descricao: 'Atividade no Portal - 03:45 PM', valor: 30.0 },
-                // Adicione mais itens ao extrato conforme necessário
-            ];
-        } else if (mesSelecionado === 'Fevereiro 2024') {
-            return [
-                { data: '05-02-2024T12:00:00', descricao: 'Atividade em Grupo - 12:00 PM', valor: 700.0 },
-                { data: '12-02-2024T14:30:00', descricao: 'Presença em sala de aula - 02:30 PM', valor: 400.0 },
-                // Adicione mais itens ao extrato de fevereiro conforme necessário
-            ];
+  const extratoData = useMemo(() => {
+    let extrato = [];
+
+    if (mesSelecionado === 'Janeiro 2024') {
+      extrato = [
+        { dia: 2, hora: '12:57 PM', historico: 'Deposito online', valor: 160.0 },
+        { dia: 10, hora: '09:00 PM', historico: 'Netflix.com', valor: -170.0 },
+        // Adiciona mais itens ao extrato de janeiro conforme necessário
+      ];
+    } else if (mesSelecionado === 'Fevereiro 2024') {
+      extrato = [
+        { dia: 5, hora: '12:00 PM', historico: 'Atividade em Grupo', valor: 150.0 },
+        { dia: 12, hora: '02:30 PM', historico: 'Presença em sala de aula', valor: 200.0 },
+        { dia: 5, hora: '12:01 PM', historico: 'Atividade em Grupo', valor: -100.0 },
+        { dia: 12, hora: '02:40 PM', historico: 'Presença em sala de aula', valor: -80.0 },
+        { dia: 5, hora: '12:57 PM', historico: 'Atividade em Grupo', valor: -120.0 },
+        { dia: 12, hora: '01:30 PM', historico: 'Presença em sala de aula', valor: -100.0 },
+        { dia: 5, hora: '09:00 PM', historico: 'Atividade em Grupo', valor: 150.0 },
+        { dia: 12, hora: '11:30 PM', historico: 'Presença em sala de aula', valor: 200.0 },
+        // Adiciona mais itens ao extrato de fevereiro conforme necessário
+      ];
+    }
+    
+    // Ordenar o extrato por data e hora
+    extrato.sort((a, b) => {
+      if (a.dia !== b.dia) {
+        return a.dia - b.dia;
+      } else {
+        // Separa as horas e minutos
+        const [aHour, aMinute] = a.hora.split(':');
+        const [bHour, bMinute] = b.hora.split(':');
+        // Compara horas e minutos
+        if (aHour !== bHour) {
+          return aHour - bHour;
+        } else {
+          return aMinute - bMinute;
         }
+      }
+    });
 
-        // Adicione mais lógica para outros meses, se necessário
-        return [];
-    }, [mesSelecionado]);
+    return extrato;
+  }, [mesSelecionado]);
 
-    const handleSelecionarMes = (event) => {
-        setMesSelecionado(event.target.value);
-    };
+  const valorTotal = useMemo(() => {
+    return extratoData.reduce((total, item) => total + item.valor, 0);
+  }, [extratoData]);
 
-    useEffect(() => {
-        const ctx = canvasRef.current;
+  const handleSelecionarMes = (event) => {
+    setMesSelecionado(event.target.value);
+  };
 
-        // Verificar se o contexto do canvas está disponível
-        if (ctx) {
-            const ctxContext = ctx.getContext('2d');
+  return (
+    <div className="extrato-saldo-top-container">
+      <div className="extrato-saldo-container">
+        <h2 className="titulo-extrato">Extrato de Moeda Digital</h2><br />
+        <p>Usuário: Israel</p>
+        <p>Matrícula: 261944</p>
+        <hr />
+        <label htmlFor="selectMes">Selecione o mês: </label>
+        <select id="selectMes" value={mesSelecionado} onChange={handleSelecionarMes}>
+          <option value="Janeiro 2024">Janeiro 2024</option>
+          <option value="Fevereiro 2024">Fevereiro 2024</option>
+          <option value="Março 2024">Março 2024</option>
+          <option value="Abril 2024">Abril 2024</option>
+          <option value="Maio 2024">Maio 2024</option>
+          <option value="Junho 2024">Junho 2024</option>
+          <option value="Julho 2024">Julho 2024</option>
+          <option value="Agosto 2024">Agosto 2024</option>
+          <option value="Setembro 2024">Setembro 2024</option>
+          <option value="Outubro 2024">Outubro 2024</option>
+          <option value="Novembro 2024">Novembro 2024</option>
+          <option value="Dezembro 2024">Dezembro 2024</option>
 
-            // Destruir gráfico anterior, se existir
-            if (myChartRef.current) {
-                myChartRef.current.destroy();
-            }
-
-            const newChart = new Chart(ctxContext, {
-                type: 'bar',
-                data: {
-                    labels: extratoData.map((item) => item.data),
-                    datasets: [
-                        {
-                            label: 'Valores',
-                            data: extratoData.map((item) => item.valor),
-                            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                            borderColor: 'rgba(75, 192, 192, 1)',
-                            borderWidth: 1,
-                        },
-                    ],
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: false,
-                            suggestedMin: 0,
-                        },
-                    },
-                    elements: {
-                        bar: {
-                            borderWidth: 1,
-                            borderColor: 'white', // Adicione a cor branca para a borda
-                        },
-                    },
-                },
-            });
-
-            // Armazenar a referência ao novo gráfico
-            myChartRef.current = newChart;
-
-            // Lembre-se de destruir o gráfico ao desmontar o componente ou quando necessário
-            return () => {
-                newChart.destroy();
-            };
-        }
-    }, [extratoData, mesSelecionado]);
-
-    return (
-        <div className="extrato-saldo-top-container">
-            <div className="extrato-saldo-container">
-                <h2>Extrato de Saldo</h2>
-                <label htmlFor="selectMes">Selecione o mês: </label>
-                <select id="selectMes" value={mesSelecionado} onChange={handleSelecionarMes}>
-                    <option value="Janeiro 2024">Janeiro 2024</option>
-                    <option value="Fevereiro 2024">Fevereiro 2024</option>
-                    <option value="Março 2024">Março 2024</option>
-                    <option value="Abril 2024">Abril 2024</option>
-                    <option value="Maio 2024">Maio 2024</option>
-                    <option value="Junho 2024">Junho 2024</option>
-                    <option value="Julho 2024">Julho 2024</option>
-                    <option value="Agosto 2024">Agosto 2024</option>
-                    <option value="Setembro 2024">Setembro 2024</option>
-                    <option value="Outubro 2024">Outubro 2024</option>
-                    <option value="Novembro 2024">Novembro 2024</option>
-                    <option value="Dezembro 2024">Dezembro 2024</option>
-                    {/* Adicione mais opções para outros meses */}
-                </select>
-
-                <p>Seu Saldo Atual: R$ {920.0.toFixed(2)} Criptomoedas</p>
-
-                <table className="extrato-tabela">
-                    <thead>
-                        <tr>
-                            <th>Data</th>
-                            <th>Descrição</th>
-                            <th>Valor</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {extratoData.map((item) => (
-                            <tr key={item.data}>
-                                <td>{item.data}</td>
-                                <td>{item.descricao}</td>
-                                <td>R$ {item.valor.toFixed(2)}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-
-                {/* Adicione um elemento de tela para o gráfico */}
-                <canvas ref={canvasRef} width="400" height="200"></canvas>
-            </div>
+          {/* Adicione mais opções para outros meses */}
+        </select>
+        <div className="extrato-tabela-container">
+          <table className="extrato-tabela">
+            <thead>
+              <tr>
+                <th>Dia</th>
+                <th>Hora</th>
+                <th>Histórico</th>
+                <th>Valor</th>
+              </tr>
+            </thead>
+            <tbody>
+              {extratoData.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.dia}</td>
+                  <td>{item.hora}</td>
+                  <td>{item.historico}</td>
+                  <td style={{ color: item.valor >= 0 ? 'blue' : 'red' }}>R$ {item.valor.toFixed(2)}</td>
+                </tr>
+              ))}
+              <tr>
+                <td colSpan="3" style={{ textAlign: 'right' }}>Valor Total:</td>
+                <td style={{ color: valorTotal >= 0 ? 'blue' : 'red' }}>
+                  R$ {valorTotal.toFixed(2)}
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ExtratoSaldo;
