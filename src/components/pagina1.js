1a33bf0339e1f19c1b8c5e8110d2f0a046c6e80f
Israel Paz <israelnetmax@gmail.com>
2024-01-21 15:00:36 -0300

    Atualização 1.0.5
    Adicionei um olho ocultar e exibir senha.

--- src/components/pagina1.js
+++ src/components/pagina1.js
@@ -2,11 +2,13 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Pagina1.css';
 import suaImagem from './softexB.png';
+import olhoIcon from './olho2-visivel.png'; // Substitua pelo caminho real do seu ícone
 
 const Pagina1 = () => {
   const navigate = useNavigate();
   const [matricula, setMatricula] = useState('');
   const [senha, setSenha] = useState('');
+  const [mostrarSenha, setMostrarSenha] = useState(false);
 
   const realizarLogin = () => {
     // Lógica para realizar o login
@@ -18,9 +20,25 @@ const Pagina1 = () => {
     }
   };
 
+// eslint-disable-next-line no-unused-vars
+const alternarVisibilidadeSenha = () => {
+  setMostrarSenha(!mostrarSenha);
+};
+
+  const handleMouseDown = () => {
+    // Ação quando o botão do mouse é pressionado
+    // Por exemplo, pode exibir a senha temporariamente
+    setMostrarSenha(true);
+  };
+
+  const handleMouseUp = () => {
+    // Ação quando o botão do mouse é solto
+    // Por exemplo, pode esconder a senha novamente
+    setMostrarSenha(false);
+  };
+
   return (
     <div className="container">
-      {/* Adicione a tag <img> aqui */}
       <img src={suaImagem} alt="Imagem no canto esquerdo superior" className="imagem-topo-esquerdo" />
 
       <div className="form-container">
@@ -30,15 +48,35 @@ const Pagina1 = () => {
           <input type="text" id="matricula" name="matricula" value={matricula} onChange={(e) => setMatricula(e.target.value)} required />
 
           <label htmlFor="senha">Senha:</label>
-          <input type="password" id="senha" name="senha" value={senha} onChange={(e) => setSenha(e.target.value)} required />
+          <div className="senha-container">
+            <input
+              type={mostrarSenha ? 'text' : 'password'}
+              id="senha"
+              name="senha"
+              value={senha}
+              onChange={(e) => setSenha(e.target.value)}
+              required
+            />
+            <img
+              src={olhoIcon}
+              alt={mostrarSenha ? 'Ocultar Senha' : 'Mostrar Senha'}
+              onMouseDown={handleMouseDown}
+              onMouseUp={handleMouseUp}
+              className="olho-icon"
+            />
+          </div>
 
-          <button type="button" onClick={realizarLogin}>Login</button>
+          <button type="button" onClick={realizarLogin}>
+            Login
+          </button>
         </form>
 
-        <p>Não tem uma conta? <Link to="/cadastro" id="inscrevaSeLink">Inscreva-se</Link></p>
+        <p>
+          Não tem uma conta? <Link to="/cadastro" id="inscrevaSeLink">Inscreva-se</Link>
+        </p>
       </div>
     </div>
   );
-}
+};
 
 export default Pagina1;
